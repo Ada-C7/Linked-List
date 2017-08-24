75da3704786a3889d589808690581c8ed52d28ed
Brenna <bcmdarroch@gmail.com>
2017-08-23 20:54:13 -0700

    first attempt

--- linked_list.rb
+++ linked_list.rb
@@ -2,11 +2,11 @@
 # Defines a node in the singly linked list
 class Node
   attr_reader :data # allow external entities to read value but not write
-  attr_accessor :next # allow external entities to read or write next node
+  attr_accessor :next_node # allow external entities to read or write next_node node
 
-  def initialize(value)
+  def initialize(value, next_node = nil)
     @data = value
-    @next = next
+    @next_node = next_node
   end
 end
 
@@ -19,58 +19,160 @@ class LinkedList
   # method to add a new node with the specific data value in the linked list
   # insert the new node at the beginning of the linked list
   def insert(value)
-    puts "Not implemented"
+    new_node = Node.new(value)
+    
+    if @head == nil
+      @head = new_node
+    else 
+      new_node.next_node = @head
+      @head = new_node
+    end
+
+    return
   end
 
   # method to find if the linked list contains a node with specified value
   # returns true if found, false otherwise
   def search(value)
-    puts "Not implemented"
+    return false if @head == nil
+
+    current = @head
+    count = 0
+
+    while current != nil
+      return true if current.data == value 
+      current = current.next_node
+    end 
+
+    return false
   end
 
   # method to return the max value in the linked list
   # returns the data value and not the node
   def find_max
-    puts "Not implemented"
+     return if @head == nil
+
+    current = @head
+    max = @head.data
+
+    while current != nil
+      max = current.data if current.data > max
+      current = current.next_node
+    end 
+
+    return max
   end
 
   # method to return the min value in the linked list
   # returns the data value and not the node
   def find_min
-    puts "Not implemented"
+     return if @head == nil
+
+    current = @head
+    min = @head.data
+
+    while current != nil
+      min = current.data if current.data < min
+      current = current.next_node
+    end 
+
+    return min
   end
 
   # method that returns the length of the singly linked list
   def length
-    puts "Not implemented"
+    return 0 if @head == nil
+
+    current = @head
+    count = 0
+
+    while current != nil
+      count += 1
+      current = current.next_node
+    end 
+
+    return count
   end
 
   # method to return the value of the nth element from the beginning
   # assume indexing starts at 0 while counting to n
   def find_nth_from_beginning(n)
-    puts "Not implemented"
+    # TODO: fix
+    # return if @head == nil
+
+    # current = @head
+    # i = 0
+
+    # for i in i...n
+    #   puts "#{i}: #{current.data}"
+    #   current = current.next_node
+    #   i += 1
+    # end 
+
+    # return current.data
   end
 
   # method to insert a new node with specific data value, assuming the linked
   # list is sorted in ascending order
   def insert_ascending(value)
-    puts "Not implemented"
+    current = @head
+    i = 0
+    
+    # while current.next_node != nil 
+    #   current = current.next_node unless value < current.next_node.data
+    # end 
+
+    current.next_node = Node.new(value, current.next_node.next_node)
+    return 
   end
 
   # method to print all the values in the linked list
   def visit
-    puts "Not implemented"
+    return if @head == nil
+
+    current = @head
+
+    while current != nil
+      puts current.data
+      current = current.next_node
+    end 
   end
 
   # method to delete the first node found with specified value
   def delete(value)
-    puts "Not implemented"
+    # TODO: fix
+    # return if @head == nil
+    # if @head.data == value
+    #   @head = @head.next_node
+    # end 
+
+    # current = @head
+    
+    # while current.next_node != nil
+    #   if current.next_node.data = value
+    #     current.next_node = current.next_node.next_node
+    #   end
+    # end
+
+    # return
   end
 
   # method to reverse the singly linked list
   # note: the nodes should be moved and not just the values in the nodes
   def reverse
-    puts "Not implemented"
+    # TODO: fix
+    # current = @head
+    # prev = nil
+    
+    # until current.data == nil
+    #   temp = current.next_node
+    #   puts "temp: #{temp.data}"
+    #   current.next_node = prev
+    #   puts "next: #{current.next_node.data}"
+    #   prev = current
+    #   puts "prev: #{prev.data}"
+    #   current = temp
+    # end
   end
 
   ## Advanced Exercises
@@ -99,11 +201,11 @@ class LinkedList
 
     # navigate to last node
     current = @head
-    while current.next != nil
-      current = current.next
+    while current.next_node != nil
+      current = current.next_node
     end
 
-    current.next = @head # make the last node link to first node
+    current.next_node = @head # make the last node link to first node
   end
 end
 
@@ -117,6 +219,10 @@ puts "Adding 5, 3 and 1 to the linked list."
 my_linked_list.insert(5)
 my_linked_list.insert(3)
 my_linked_list.insert(1)
+
+# validates search
+puts "5 is in the linked list: #{my_linked_list.search(5)}"
+puts "100 is in the linked list: #{my_linked_list.search(100)}"
 
 # print all elements
 puts "Printing elements in the linked list:"
@@ -165,7 +271,7 @@ puts "Confirming min and max values in the linked list."
 min = my_linked_list.find_min
 puts "BUG: Min value should be 1 and not #{min}" if min != 1
 max = my_linked_list.find_max
-puts "BUG: Max value should be 5 and not #{max}" if max != 6
+puts "BUG: Max value should be 6 and not #{max}" if max != 6
 
 # delete value
 puts "Deleting node with value 5 from the linked list."
