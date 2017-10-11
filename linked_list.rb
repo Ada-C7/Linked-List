ab05179672b2e6476940f3859eec0a43b85d5a05
Valliere, Andrea <andrea.valliere@nordstrom.com>
2017-10-10 21:14:39 -0700

    Removed merge conflict messages

--- linked_list.rb
+++ linked_list.rb
@@ -34,7 +34,6 @@ class LinkedList
   # returns true if found, false otherwise
   def search(value)
     # puts "Not implemented"
-<<<<<<< HEAD
     if @head == nil
       return false
     end
@@ -46,7 +45,6 @@ class LinkedList
         current = current.next
       end
       return false
-=======
     current = @head
     while current.next != nil
       if current.data == value
@@ -54,7 +52,6 @@ class LinkedList
       else
         current = current.next
       end
->>>>>>> 75eb551ceeec890b5b6ce686e164095787cee6b1
     end
   end
 
@@ -63,7 +60,6 @@ class LinkedList
   def find_max
     # puts "Not implemented"
     current = @head
-<<<<<<< HEAD
     max = @head
     until current.next == nil
       if current.next.data > max.data
@@ -72,7 +68,6 @@ class LinkedList
         current = current.next
     end
       return max.data
-=======
     max = current.data
     while current.next != nil
       if max < current.next.data
@@ -81,7 +76,6 @@ class LinkedList
       current = current.next
     end
     return max
->>>>>>> 75eb551ceeec890b5b6ce686e164095787cee6b1
   end
 
   # method to return the min value in the linked list
@@ -89,7 +83,6 @@ class LinkedList
   def find_min
     # puts "Not implemented"
     current = @head
-<<<<<<< HEAD
     min = @head
     until current.next == nil
       if current.next.data < min.data
@@ -98,7 +91,6 @@ class LinkedList
       current = current.next
     end
       return min.data
-=======
     min = current.data
     while current.next != nil
       if min > current.next.data
@@ -107,7 +99,6 @@ class LinkedList
       current = current.next
     end
     return min
->>>>>>> 75eb551ceeec890b5b6ce686e164095787cee6b1
   end
 
   # method that returns the length of the singly linked list
@@ -131,22 +122,19 @@ class LinkedList
   def find_nth_from_beginning(n)
     # puts "Not implemented"
     current = @head
-<<<<<<< HEAD
-    n.times do 
+    n.times do
       current = current.next
     end
     return current.data
-=======
     i = 0
     while current != nil
-        if i != n
-          i += 1
-          current = current.next
-        else
-          return current.data
-        end
+      if i != n
+        i += 1
+        current = current.next
+      else
+        return current.data
+      end
     end
->>>>>>> 75eb551ceeec890b5b6ce686e164095787cee6b1
   end
 
   # method to insert a new node with specific data value, assuming the linked
@@ -172,9 +160,6 @@ class LinkedList
   # method to delete the first node found with specified value
   def delete(value)
     # puts "Not implemented"
-<<<<<<< HEAD
-=======
-
     current = @head
     while current != nil
       if @head == nil
@@ -187,7 +172,6 @@ class LinkedList
         current = current.next
       end
     end
->>>>>>> 75eb551ceeec890b5b6ce686e164095787cee6b1
   end
 
 
@@ -195,8 +179,6 @@ class LinkedList
   # note: the nodes should be moved and not just the values in the nodes
   def reverse
     # puts "Not implemented"
-<<<<<<< HEAD
-=======
     current = @head
     previous = nil
 
@@ -207,7 +189,6 @@ class LinkedList
       current = temp
     end
     @head = previous
->>>>>>> 75eb551ceeec890b5b6ce686e164095787cee6b1
   end
 
   ## Advanced Exercises
@@ -259,11 +240,8 @@ my_linked_list.insert(1)
 puts "Printing elements in the linked list:"
 my_linked_list.visit
 #
-<<<<<<< HEAD
 # # Find the value at the nth node
-=======
 # Find the value at the nth node
->>>>>>> 75eb551ceeec890b5b6ce686e164095787cee6b1
 puts "Confirming values in the linked list using find_nth_from_beginning method."
 value = my_linked_list.find_nth_from_beginning(2)
 puts "BUG: Value at index 2 should be 5 and is #{value}" if value != 5
@@ -297,34 +275,28 @@ puts "BUG: Value at index 0 should be 1 and is #{value}" if value != 1
 # my_linked_list.visit
 #
 # vaidate length
-<<<<<<< HEAD
 # puts "Confirming length of the linked list."
 # my_linked_list_length = my_linked_list.length
 # puts "BUG: Length should be 5 and not #{my_linked_list_length}" if my_linked_list_length != 5
-=======
 puts "Confirming length of the linked list."
 my_linked_list_length = my_linked_list.length
 puts "BUG: Length should be 5 and not #{my_linked_list_length}" if my_linked_list_length != 5
->>>>>>> 75eb551ceeec890b5b6ce686e164095787cee6b1
 #
 # find min and max
 puts "Confirming min and max values in the linked list."
 min = my_linked_list.find_min
 puts "BUG: Min value should be 1 and not #{min}" if min != 1
 max = my_linked_list.find_max
-<<<<<<< HEAD
 puts "BUG: Max value should be 5 and not #{max}" if max != 5
 #
 # # delete value
 # puts "Deleting node with value 5 from the linked list."
 # my_linked_list.delete(5)
-=======
 puts "BUG: Max value should be 5 and not #{max}" if max != 6
 #
 # delete value
 puts "Deleting node with value 5 from the linked list."
 my_linked_list.delete(5)
->>>>>>> 75eb551ceeec890b5b6ce686e164095787cee6b1
 # # print all elements
 # puts "Printing elements in the linked list:"
 # my_linked_list.visit
@@ -349,15 +321,12 @@ my_linked_list.delete(5)
 # middle = my_linked_list.find_middle_value
 # puts "BUG: Middle value should be 4 and not #{middle}" if middle != 4
 #
-<<<<<<< HEAD
 # # reverse the linked list
 # puts "Reversing the linked list."
 # my_linked_list.reverse
-=======
 # reverse the linked list
 puts "Reversing the linked list."
 my_linked_list.reverse
->>>>>>> 75eb551ceeec890b5b6ce686e164095787cee6b1
 # # print all elements
 # puts "Printing elements in the linked list:"
 # my_linked_list.visit
